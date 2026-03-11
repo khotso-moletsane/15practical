31f268a8e33ab1e35460ec8a0973175511a66f4e
sbonga <4323535@myuwc.ac.za>
2026-03-11 14:56:06 +0200

    Add cleanWord method to clean punctuation from words

--- Anagrams.java
+++ Anagrams.java
@@ -1,3 +1,12 @@
 public class Anagrams {
-    
+
+    // remove punctuation and convert to lowercase
+    static String cleanWord(String w) {
+
+        w = w.toLowerCase();           // convert word to lowercase
+        w = w.replaceAll("[^a-z']", ""); // remove punctuation
+
+        return w;
+    }
+
 }
